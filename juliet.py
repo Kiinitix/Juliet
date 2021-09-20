01288378092e991103ade16fdbdfd0d35539fec8
Kabir Dhruw <34811605+Kiinitix@users.noreply.github.com>
2021-09-20 16:32:49 +0530

    Update juliet.py

--- juliet.py
+++ juliet.py
@@ -11,15 +11,17 @@ df = pd.read_csv('test.txt',sep='\t')
 a = pd.Series(df.columns)
 a = a.rename({0: df.columns[0],1: df.columns[1]})
 
+def cleaner(x):
+    return [a for a in (''.join([a for a in x if a not in string.punctuation])).lower().split()]
+    
 Pipe = Pipeline([
     ('bow',CountVectorizer(analyzer=cleaner)),
     ('tfidf',TfidfTransformer()),
     ('classifier',DecisionTreeClassifier())
 ])
 
-Pipe.fit(df['Questions'],df['Answers'])
+Pipe.fit(df['question'],df["answer"])
 
 while(True):
-          a=input()
-          Pipe.predict([a])[0]
-          
+          a=input("User: ")
+          print("Juliet: ", Pipe.predict([a])[0])
